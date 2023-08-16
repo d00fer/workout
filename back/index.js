5e34a2508d79a5f69a5566d10d1373bb50f1e061
Patrycjusz Myszka <pat.myszka@gmail.com>
2023-08-16 06:34:56 +0100

    cos

--- back/index.js
+++ back/index.js
@@ -5,11 +5,6 @@ import workoutRoutes from './routes/workouts.js'
 const app = express()
 const port = process.env.PORT || 2000
 
-app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
-
 app.use(workoutRoutes)
 
 app.listen(port)
