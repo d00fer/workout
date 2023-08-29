5b931e6d10dc37c27b6888a7d43bd85503b40b89
Patrycjusz Myszka <pat.myszka@gmail.com>
2023-08-29 10:40:48 +0100

    add some midd

--- back/index.js
+++ back/index.js
@@ -3,8 +3,16 @@ import express from 'express'
 import workoutRoutes from './routes/workouts.js'
 
 const app = express()
+
 const port = process.env.PORT || 2000
 
-app.use(workoutRoutes)
+//Just some example of middleware
+//printing url path in to the node js console.log()
+app.use((req, res, next) => {
+  console.log(req.path)
+  next()
+})
+
+app.use('/api/workouts', workoutRoutes)
 
 app.listen(port)
--- back/routes/workouts.js
+++ back/routes/workouts.js
@@ -1,9 +1,13 @@
 import express from 'express'
 
 const router = express.Router()
-
+//GET all workouts
 router.get('/', (req, res) => {
-  res.json({ mess: 'cos' })
+  res.json({ msg: 'all workouts!' })
+})
+
+router.get('/:id', (req, res) => {
+  res.json({ msg: 'specific workout' })
 })
 
 export default router
