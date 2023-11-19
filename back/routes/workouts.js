e9ddd57b5677196618d647d2908f450f41902456
Patrycjusz Myszka <pat.myszka@gmail.com>
2023-11-19 14:56:52 +0000

    cc

--- back/routes/workouts.js
+++ back/routes/workouts.js
@@ -8,15 +8,11 @@ import {
 } from '../controllers/workoutControllers.js'
 
 const router = express.Router()
-//get all workouts
+
 router.get('/', getWorkouts)
-//get workout by id
 router.get('/:id', getWorkout)
-//post a new workout
 router.post('/', createWorkout)
-
 router.delete('/:id', deleteWorkout)
-
 router.patch('/:id', updateWorkout)
 
 export default router
